3d1d0bd022b76fe8da642a780bbc1f2caeb783a9
Paul Cavallo <greendrone246@gmail.com>
2020-11-24 23:12:26 -0800

    Test

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI;
+var mongoDB = 'mongodb+srv://admin:passwordpassword@cluster0.bqr5n.mongodb.net/local_library?retryWrites=true';
+// var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
