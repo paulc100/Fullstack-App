3bafc19f77a41c9b667cba27476128d2d525d0b6
Paul Cavallo <greendrone246@gmail.com>
2020-11-24 22:10:43 -0800

    First

---
+++ models/author.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  if (this.date_of_death) {
+    var lifespan = (this.date_of_death.getYear() - this.date_of_birth.getYear())
+    return lifespan;
+  }
+  else {
+    return "Alive";
+  }
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
